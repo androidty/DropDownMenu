45f228db07412ab9a4562827d22b8d74aa6c609d
ty <389170487@qq.com>
2020-05-07 18:03:12 +0800

    fix

--- app/src/main/java/com/ty/dropdowndemo/DropMenuActivity.kt
+++ app/src/main/java/com/ty/dropdowndemo/DropMenuActivity.kt
@@ -2,14 +2,12 @@ package com.ty.dropdowndemo
 
 import android.content.Context
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.PopupWindow
 import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ty.dropdowndemo.bean.DropBean
@@ -19,8 +17,8 @@ import com.ty.listener.OnMenuClickListener
 import kotlinx.android.synthetic.main.activity_dropmenu.*
 
 class DropMenuActivity : AppCompatActivity() {
-    val strings = arrayOf("选择城市选择城市", "选择性别", "选择年龄","价格趋势", "价格趋势")
-    val arr1 = arrayOf("全部城市", "北京", "上海", "广州", "深圳","杭州")
+    val strings = arrayOf("选择城市选择城市", "选择性别", "选择年龄", "价格趋势", "价格趋势")
+    val arr1 = arrayOf("全部城市", "北京", "上海", "广州", "深圳", "杭州")
     val arr2 = arrayOf("性别", "男", "女")
     val arr3 = arrayOf("全部年龄", "10", "20", "30", "40", "50", "60", "70", "80", "90")
 
@@ -61,7 +59,7 @@ class DropMenuActivity : AppCompatActivity() {
     }
 
     private fun initDropDownMenu() {
-        mDropDownMenu?.initMenu(strings, false)
+        mDropDownMenu?.initMenu(strings, true)
         mDropDownMenu?.onMenuClickListener = object : OnMenuClickListener {
             override fun onMenuClickListener(context: Context, index: Int) {
                 setDropList(context, index)
@@ -70,57 +68,65 @@ class DropMenuActivity : AppCompatActivity() {
     }
 
     fun setDropList(context: Context, index: Int) {
-
-        if (mDropDownView == null) {
-            mDropDownView = LayoutInflater.from(this).inflate(R.layout.popupwindow_menu, null)
-        }
-        if (mShadowRl == null) {
-            mShadowRl = mDropDownView!!.findViewById(R.id.rl_menu_shadow)
-            mShadowRl!!.setBackgroundColor(Color.parseColor("#11000000"))
-            mShadowRl?.setOnClickListener{mDropDownMenu.dismiss()}
-        }
-        if (mDropRlv == null) {
-            mDropRlv = mDropDownView!!.findViewById(R.id.mDropRlv)
-        }
-        mDropDownMenu?.setDropWindow(mDropDownView!!)
-        if (mDropRlv!!.itemDecorationCount == 0) {
-            mDropRlv!!.addItemDecoration(MyDividerItemDecoration(this, null, 1, 15, 15, true))
-        }
-        mDropRlv!!.layoutManager = LinearLayoutManager(this)
-        if (dropAdapter == null) {
-            dropAdapter = MyAdapter(R.layout.menu_list_item, dropBeans1)
-        }
-        mDropRlv!!.adapter = dropAdapter
-
-
         when (index) {
-            0 -> {
-                dropAdapter?.setNewData(dropBeans1)
-            }
-            1 -> {
-                dropAdapter?.setNewData(dropBeans2)
-            }
-            2 -> {
-                dropAdapter?.setNewData(dropBeans3)
+            3 -> {
+                initCustomMenu(context,index);
             }
             else -> {
-                dropAdapter?.setNewData(dropBeans1)
-            }
-        }
+                if (mDropDownView == null) {
+                    mDropDownView = LayoutInflater.from(this).inflate(R.layout.popupwindow_menu, null)
+                }
+                if (mShadowRl == null) {
+                    mShadowRl = mDropDownView!!.findViewById(R.id.rl_menu_shadow)
+                    mShadowRl!!.setBackgroundColor(Color.parseColor("#11000000"))
+                    mShadowRl?.setOnClickListener { mDropDownMenu.dismiss() }
+                }
+                if (mDropRlv == null) {
+                    mDropRlv = mDropDownView!!.findViewById(R.id.mDropRlv)
+                }
+                mDropDownMenu?.setDropWindow(mDropDownView!!)
+                if (mDropRlv!!.itemDecorationCount == 0) {
+                    mDropRlv!!.addItemDecoration(MyDividerItemDecoration(this, null, 1, 15, 15, true))
+                }
+                mDropRlv!!.layoutManager = LinearLayoutManager(this)
+                if (dropAdapter == null) {
+                    dropAdapter = MyAdapter(R.layout.menu_list_item, dropBeans1)
+                }
+                mDropRlv!!.adapter = dropAdapter
 
-        //下拉列表点击事件
-        dropAdapter!!.setOnItemClickListener { adapter, view, position ->
-            (adapter.data[position] as DropBean).name.let { mDropDownMenu.setCurrentTitle(index, it!!) }
-            when (index) {
-                0 -> option1 = (adapter.data[position] as DropBean).name
-                1 -> option2 = (adapter.data[position] as DropBean).name
-                2 -> option3 = (adapter.data[position] as DropBean).name
+
+                when (index) {
+                    0 -> {
+                        dropAdapter?.setNewData(dropBeans1)
+                    }
+                    1 -> {
+                        dropAdapter?.setNewData(dropBeans2)
+                    }
+                    2 -> {
+                        dropAdapter?.setNewData(dropBeans3)
+                    }
+                    else -> {
+                        dropAdapter?.setNewData(dropBeans1)
+                    }
+                }
+
+                //下拉列表点击事件
+                dropAdapter!!.setOnItemClickListener { adapter, view, position ->
+                    (adapter.data[position] as DropBean).name.let { mDropDownMenu.setCurrentTitle(index, it!!) }
+                    when (index) {
+                        0 -> option1 = (adapter.data[position] as DropBean).name
+                        1 -> option2 = (adapter.data[position] as DropBean).name
+                        2 -> option3 = (adapter.data[position] as DropBean).name
+                    }
+                    setSelect(position, adapter.data as List<DropBean>)
+                    mDropDownMenu.dismiss()
+                    Toast.makeText(DropMenuActivity@ this, "$option1 $option2 $option3", Toast.LENGTH_LONG).show()
+                }
             }
-            setSelect(position, adapter.data as List<DropBean>)
-//            mPopupWindow!!.dismiss()
-            mDropDownMenu.dismiss()
-            Toast.makeText(DropMenuActivity@ this, "$option1 $option2 $option3", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun initCustomMenu(context: Context, index: Int) {
     }
 
     private fun setSelect(pos: Int, data: List<DropBean>) {
